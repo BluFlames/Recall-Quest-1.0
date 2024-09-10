697910190ea646d5cd48e6bff83c8acbbc426f46
mahesh <mahesh@gmail.com>
2024-09-10 14:33:19 +0530

    Testing git commit

--- app/src/main/java/com/example/recallquest10/MainActivity.java
+++ app/src/main/java/com/example/recallquest10/MainActivity.java
@@ -3,5 +3,6 @@ package com.example.recallquest10;
 public class MainActivity {
     public static void main (String [] args){
 System.out.println("Hello, it's my first application");
+System.out.println("Hi");
     }
 }
