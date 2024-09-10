6b420f7450498fa786b2952548f75ef04673d028
Ravi Kumara M <ravipersonal8123@gmail.com>
2024-09-10 16:05:22 +0530

    Update MainActivity.java

--- app/src/main/java/com/example/recallquest10/MainActivity.java
+++ app/src/main/java/com/example/recallquest10/MainActivity.java
@@ -3,5 +3,6 @@ package com.example.recallquest10;
 public class MainActivity {
     public static void main (String [] args){
 System.out.println("Hello it's my first App using Java");
+        System.out.println("jfjfjfjfjfjfjfj");
     }
 }
