d22d515ecce7ea6fd624bcabc99dfdef1c2ce27a
HarshaJE <harshaje69@gmail.com>
2024-10-12 16:00:25 +0530

    Working App in Beta 2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,7 +10,7 @@ android {
     defaultConfig {
         applicationId = "com.miniproject.recallquest10"
         minSdk = 28
-        targetSdk = 28
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
