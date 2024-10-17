f63c60913db9f27f866051bcd87687bf65de282f
HarshaJE <harshaje69@gmail.com>
2024-10-17 17:16:13 +0530

    S'more

---
+++ app/src/main/java/com/example/recallquest10/HostActivity.java
@@ -0,0 +1,13 @@
+package com.example.recallquest10;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class HostActivity extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.a7_host);
+        setContentView(R.layout.a3_choose_levels);
+    }
+}
--- app/src/main/java/com/example/recallquest10/MultiplayerActivity.java
+++ app/src/main/java/com/example/recallquest10/MultiplayerActivity.java
@@ -1,12 +1,29 @@
 package com.example.recallquest10;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+
 import androidx.appcompat.app.AppCompatActivity;
+
 public class MultiplayerActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.a5_multiplayer);
+
+        Button hostButton = findViewById(R.id.host_button);
+        Button joinButton = findViewById(R.id.join_button);
+
+        hostButton.setOnClickListener(v -> {
+            Intent intent = new Intent(MultiplayerActivity.this, HostActivity.class);
+            startActivity(intent);
+        });
+
+        joinButton.setOnClickListener(v -> {
+            Intent intent = new Intent(MultiplayerActivity.this, HostActivity.class);
+            startActivity(intent);
+        });
     }
 }
