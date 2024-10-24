7ef7406d48222c26756401fc63e431dfdb85be49
HarshaJE <harshaje69@gmail.com>
2024-10-24 10:00:33 +0530

    S'more

--- app/src/main/java/com/example/recallquest10/HostActivity.java
+++ app/src/main/java/com/example/recallquest10/HostActivity.java
@@ -7,7 +7,7 @@ import androidx.appcompat.app.AppCompatActivity;
 public class HostActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.a7_host);
+//        setContentView(R.layout.a7_host);
         setContentView(R.layout.a3_choose_levels);
     }
 }
